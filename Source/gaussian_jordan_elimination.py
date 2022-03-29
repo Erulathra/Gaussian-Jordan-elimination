a5924db1542d730f4ee752774c8820d89ecc64e6
Szymon Świędrych <szymek_sw@outlook.com>
2022-03-29 17:07:00 +0200

    Create Jordan elimination function

---
+++ Source/gaussian_jordan_elimination.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+# czy trzeba macierz odwrotną w Jordanie;
+# czy trzeba umieć liczyć macierze nie kwadratowe;
+class EquationType:
+    unique = "Unique"
+    infinite = "Infinite"
+    none = "None"
+
+
+def check_is_result_has_multiples(matrix: np.ndarray, result: np.ndarray) -> bool:
+    glued_matrix = matrix
+    glued_matrix = np.append(glued_matrix, np.matrix(result).T, axis=1)
+    glued_matrix = np.array(glued_matrix)
+
+    for i in range(result.size):
+        for j in range(i + 1, result.size):
+            correlation = glued_matrix[i] / glued_matrix[j]
+            a = np.all(correlation == correlation[0])
+            if 1 == 0:
+                return True
+
+    return False
+
+
+def solve_matrix(matrix: np.ndarray) -> (EquationType, np.ndarray):
+    matrix = np.array(matrix.copy())
+
+    result_vector: np.ndarray = matrix[:, -1]
+    matrix: np.ndarray = np.delete(matrix, -1, axis=1)
+
+    if result_vector.shape[0] < matrix.shape[0]:
+        return EquationType.infinite
+
+    if np.linalg.det(matrix) == 0:
+        if check_is_result_has_multiples(matrix, result_vector):
+            return EquationType.infinite, result_vector
+        else:
+            return EquationType.none, result_vector
+
+    for i in range(matrix.shape[1]):
+        factor = matrix[i, i]
+
+        # Transform row to have one on diagonal
+        matrix[i] /= factor
+        result_vector[i] /= factor
+        # Subtract this row from rest of the rows
+        for j in range(i + 1, matrix.shape[1]):
+            factor = matrix[j, i]
+            matrix[j] -= matrix[i] * factor
+            result_vector[j] -= result_vector[i] * factor
+
+    # Subtract rows from end to make matrix diagonal
+    for i in range(matrix.shape[1])[::-1]:
+        for j in range(i):
+            factor = matrix[j, i]
+            matrix[j] -= matrix[i] * factor
+            result_vector[j] -= result_vector[i] * factor
+
+    return EquationType.unique, result_vector
---
+++ Source/main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import gaussian_jordan_elimination as jordan
+
+
+def main():
+    common_matrix = np.array([[3, 3, 1, 12],
+                              [2, 5, 7, 33],
+                              [1, 2, 1, 8]], float)
+
+    infinite_results_matrix = np.array([[3, 3, 1, 1],
+                                        [2, 5, 7, 20],
+                                        [-4, -10, -14, -40]], float)
+
+    none_results_matrix = np.array([[3, 3, 1, 20],
+                                    [2, 5, 7, 20],
+                                    [-4, -10, -14, -20]], float)
+
+    result = jordan.solve_matrix(common_matrix)
+    print(result)
+
+    result = jordan.solve_matrix(infinite_results_matrix)
+    print(result)
+
+    result = jordan.solve_matrix(none_results_matrix)
+    print(result)
+
+    A = np.array([2, 4, 6])
+    B = np.array([1, 2, 3])
+
+    print(B / A)
+
+
+if __name__ == '__main__':
+    main()
