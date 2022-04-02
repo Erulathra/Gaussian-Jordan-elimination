344e33fe5b3cf5d77f0bfad67312057064472b5b
236669 <236669@edu.p.lodz.pl>
2022-04-02 14:19:53 +0200

    Start making CLI interface; Add examples from wikamp

--- Source/main.py
+++ Source/main.py
@@ -1,16 +1,20 @@
 import numpy as np
+from read_file import matrix_from_file
 import gaussian_jordan_elimination as jordan
 
 
 def calculate_and_print_results(matrix):
+    print("\nMacierz wejściowa:\n", matrix)
     try:
         result = jordan.solve_matrix(matrix)
+        print("Wynik:")
         print(result[0])
         print(result[1])
     except jordan.NoneSolutionException:
         print("Układ sprzeczny")
     except jordan.InfiniteSolutionException:
         print("Układ nieoznaczony")
+    print()
 
 
 def main():
@@ -26,9 +30,33 @@ def main():
                                     [2, 5, 7, 20],
                                     [-4, -10, -14, -20]], float)
 
-    calculate_and_print_results(common_matrix)
-    calculate_and_print_results(infinite_results_matrix)
-    calculate_and_print_results(none_results_matrix)
+    
+    user_input = ""
+    while user_input != "q":
+        user_input = input('''Podaj, czy chcesz obliczyć rozwiązanie układu:
+        (1) Oznaczonego
+        (2) Sprzecznego
+        (3) Nieoznaczonego
+        (4) Własnego (wczytaj z pliku)
+        [q - wyjście]\n> ''')
+        match(user_input):
+            case '1':
+                matrix = common_matrix
+            case '2':
+                matrix = none_results_matrix
+            case '3':
+                matrix = infinite_results_matrix
+            case '4':
+                if 'path' not in locals():
+                    path = "file.csv"
+                path = input("Podaj ścieżkę ["+path+"]: ") or path
+                matrix = matrix_from_file(path)
+            case 'q':
+                break
+            case _:
+                continue
+        calculate_and_print_results(matrix)
+                
 
 
 if __name__ == '__main__':
--- Source/read_file.py
+++ Source/read_file.py
@@ -6,10 +6,5 @@ def matrix_from_file(path) -> np.array:
         reader = csv.reader(csv_file)
         result = []
         for line in reader:
-            result.append([int(char) for char in line])
-    return np.array(result)
-
-
-#DELETE THIS
-arr = matrix_from_file("file.csv")
-print(arr)
+            result.append([float(char) for char in line])
+    return np.array(result, float)
